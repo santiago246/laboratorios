5d96115ec7da593231ea3f7868d97ce514be6181
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2017-03-24 11:11:07 -0500

    proyecto

--- practica 1/main.cpp
+++ practica 1/main.cpp
@@ -19,14 +19,47 @@ int main()
     cout<<"ingrese la altura del triangulo:";
     cin>>a;
     cout<<"el area del triangulo es:"<<(b*a)/2<<endl;
-    */
-    int x,y,c;
+
+    int x,y;
     x=11;
     y=20;
-    c=x;
-    x=y;
-    y=c;
+    x=x+y
+    y=x-y
+    x=x-y
     cout<<"y="<<y<<endl;
     cout<<"x="<<x<<endl;
+
+    int a,b;
+    cout<<"ingrese dos numeros a comparar"<<endl;
+    cin>>a;
+    cin>>b;
+    if (a!=b){
+        if(a>b)
+            cout<<a<<"es el mayor"<<endl;
+        else
+            cout<<b<<"es el mayor"<<endl;
+    }
+    else:
+            cout<<"son iguales"<<endl;
+    */
+    int a,b,c;
+    cout<<"ingrese dos numeros a comparar"<<endl;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    if (a!=b and a!=c and b!=c){
+        if(a>b and a>c)
+            cout<<a<<" es el mayor"<<endl;
+        else{
+            if(b>c){
+                cout<<b<<" es el mayor"<<endl;
+            }
+            else
+                cout<<c<<" es el mayor"<<endl;
+        }
+    }
+    else
+            cout<<" son iguales"<<endl;
+
     return 0;
 }
