5a38308f2845398599c7f6626ccb48f152be7bc8
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2017-03-24 10:44:31 -0500

    proyecto

---
+++ practica 1/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    float pi=2.1416;
+    float a,b;
+    cout<<"ingrese el radio del circulo:";
+    cin>>a;
+    cout<<"el area del circulo es:"<<pi*(a*a)<<endl;
+    cout<<"ingrese el lado del cuadrado:";
+    cin>>a;
+    cout<<"el area del cuadrado es:"<<a*a<<endl;
+    cout<<"el perimetro del cuadrado es:"<<a*4<<endl;
+    cout<<"ingrese la base del triangulo:";
+    cin>>b;
+    cout<<"ingrese la altura del triangulo:";
+    cin>>a;
+    cout<<"el area del triangulo es:"<<(b*a)/2<<endl;
+    */
+    int x,y,c;
+    x=11;
+    y=20;
+    c=x;
+    x=y;
+    y=c;
+    cout<<"y="<<y<<endl;
+    cout<<"x="<<x<<endl;
+    return 0;
+}
